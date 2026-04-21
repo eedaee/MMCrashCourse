ab37993f6eadb3f3e0a413c6fd2f0ba710f97d15
eedaee <eaespino@uci.edu>
2026-04-21 11:25:19 -0700

    Latest updates on Tues. April 21 @ 11:24 am. Very minor updates to main.cpp

--- software/main.cpp
+++ software/main.cpp
@@ -1,3 +1,46 @@
+#include <iostream>
+using namespace std;
+
+// Structure (Coordinates and Cell Values) and Enum (Direction) for mouse
+struct Coord{
+  int x;
+  int y;
+};
+
+enum Direction {
+  NORTH, 
+  EAST, 
+  SOUTH, 
+  WEST
+};
+
+struct Cell{
+  
+};
+
+
+struct Maze{
+  int distances[16][16];
+  int cellWalls[16][16];
+  Coord* goalPos;
+};
+
+
 int main(){
+  
+  
+  
   return 0;
 }
+
+/*
+OLD TEST CODE
+  int arr[4] = {NORTH, EAST, SOUTH, WEST};
+
+  for(int i=0; i<4; i++){
+    cout << arr[i] << endl;
+  }
+  
+  return 0;
+  */
+  
